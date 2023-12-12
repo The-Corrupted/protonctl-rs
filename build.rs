2fda5e4db7cd138969b2c426cba6e52401185a4f
The-Corrupted <intothedark.me@gmail.com>
2023-12-12 03:28:38 -0800

    Quick hack to get completion script(s) generated
    This adds a build script to generate completions at compile time and
    makes a copy of the build_cmd code in a completions file. We've created
    the second file because trying to include! the cmd file in the build
    script seems to break it in some way.

---
+++ build.rs
@@ -0,0 +1,24 @@
+use std::io::Error;
+use std::env;
+use clap_complete::generate_to;
+use clap_complete_nushell::Nushell;
+
+include!("src/cli_completion.rs");
+
+fn main() -> Result<(), Error> {
+    let outdir = match env::var_os("OUT_DIR") {
+        None => return Ok(()),
+        Some(out) => out,
+    };
+
+    let mut cmd = build_cli();
+    let path = generate_to(
+        Nushell,
+        &mut cmd,
+        "protonctl",
+        outdir
+        )?;
+    println!("cargo:warning=completion file is generated: {path:?}");
+
+    Ok(())
+}
